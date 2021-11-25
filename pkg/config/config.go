1383983d192b09e1f2914483c3e66748d1969bf7
GeekMuch <amardjebbara@gmail.com>
2021-11-25 11:19:15 +0100

    Outcommented reboot pi

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -207,7 +207,7 @@ func UpdateConfig(conf model.PiConfResponse) error{
 		if err := updateHostname(conf.Hostname); err != nil {
 			log.Logger.Warn().Msgf("[X]\tError Changing Hostname: %s", err)
 		}
-		//rebootFlag = true
+		rebootFlag = true
 
 		//todo Set hostname in respective files with func
 	}
