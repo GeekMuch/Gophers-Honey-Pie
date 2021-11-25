e523b6042d6587f5d2a1741e7337bad8bb36ff9f
GeekMuch <amardjebbara@gmail.com>
2021-11-25 15:55:16 +0100

    Added mac changing

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -117,7 +117,8 @@ func getNICVendorList() error {
 	cmd := exec.Command("wget", "http://standards-oui.ieee.org/oui/oui.csv", "-O", "NICVendors/vendors.csv")
 	err := cmd.Run()
 	if err != nil {
-		fmt.Printf("[X]\tError in putting down, command  %s", err)
+		log.Logger.Warn().Msgf("[X]\tError in getNICVendor list, command  %s", err)
+
 		return err
 	}
 	return nil
@@ -138,7 +139,7 @@ func readNICVendorFile(NICVendor string) string {
 			break
 		}
 		if err != nil {
-			log.Logger.Fatal().Msgf("[X]\tError in reading Vendor CSV file %s",err)
+			log.Logger.Warn().Msgf("[X]\tError in reading Vendor CSV file %s",err)
 		}
 		if strings.Contains(column[2], NICVendor) {
 			tmpMAC = column[1]
