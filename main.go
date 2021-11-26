3ac7e3090cfdeb2962a7fe2156fe89ce0219f4cb
GeekMuch <amardjebbara@gmail.com>
2021-11-26 10:41:09 +0100

    Added mac changing

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/GeekMuch/Gophers-Honey-Pie/pkg/api"
 	"github.com/GeekMuch/Gophers-Honey-Pie/pkg/config"
 	"github.com/GeekMuch/Gophers-Honey-Pie/pkg/honeypots"
+	"sync"
 
 	//"github.com/GeekMuch/Gophers-Honey-Pie/pkg/honeypots/opencanary"
 
@@ -25,14 +26,24 @@ func main() {
 		log.Logger.Fatal().Msgf("Error initializing honeypots: %s", err)
 		return
 	}
+	var wg sync.WaitGroup
 
-	go api.GetConfFromBackend()
-	go api.Heartbeat()
+	wg.Add(1)
+	go func() {
+		api.GetConfFromBackend()
+		log.Logger.Warn().Msgf("GetConfFromBackend goroutine done")
+		wg.Done()
+	}()
+	wg.Add(1)
+	go func() {
+		api.Heartbeat()
+		log.Logger.Warn().Msgf("Heartbeat goroutine done")
+		wg.Done()
+	}()
 	//opencanary.ReadFromToCanaryConfig()
 	//opencanary.WriteToCanaryConfigFile()
 	//opencanary.Start()
-	for {
-	}
+	wg.Wait()
 
 	// api.GetDeviceIDFromAPI()
 	// config.AddDeviceIDtoYAML()
