8f629acab2f3c2f8c93d30b0ed1afa498626ed73
GeekMuch <amardjebbara@gmail.com>
2021-11-26 16:19:23 +0100

    Added service for macchange

---
+++ gophers-macchanger.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Gets latest update from Github
+echo "[ + ] Get mac from /boot/config.yml"
+MAC=$(cat /boot/config.yml | grep -Eo "\w+:\w+:\w+:\w+:\w+:\w+")
+echo "[ ! ] Changing MAC Address"
+macchanger --mac $MAC eth0
